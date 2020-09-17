8a7a018780c6182c339c3689d9d5b02a1f78ffe6
Lyle <jover.lyle@gmail.com>
2020-09-17 16:56:53 -0500

    Initial Commit

---
+++ SwiftUIPlayground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIPlayground
+//
+//  Created by Lyle Jover on 9/17/20.
+//  Copyright © 2020 craftycoders.io. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
